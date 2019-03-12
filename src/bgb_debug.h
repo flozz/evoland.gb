42753e1630b2a66fac954ad795c1051d2a1c29f9
Fabien LOISON <flo@flogisoft.com>
2019-03-12 12:55:34 +0100

    Fix debug macros that breaks loops

--- src/bgb_debug.h
+++ src/bgb_debug.h
@@ -1,22 +1,22 @@
 #ifdef DEBUG
 
 
-#define CONCAT_(A, B) A ## B
-#define CONCAT(A, B) CONCAT_(A, B)
+#define CONCAT_(A, B, C) A ## B ## C
+#define CONCAT(A, B, C) CONCAT_(A, B, C)
 
-#define LOG_UNIQUE_LABEL CONCAT(LOG_, __COUNTER__)
+#define LOG_UNIQUE_LABEL CONCAT(42424200, __COUNTER__, $)
 
 // Log a message to the BGB console
-#define BGB_LOG(MESSAGE)  \
+#define BGB_LOG_(LABEL, MESSAGE)  \
 __asm                 \
-    LOG_UNIQUE_LABEL: \
     .db 0x52          \
-    jr 1$             \
+    jr LABEL          \
     .dw 0x6464        \
     .dw 0x0000        \
     .str MESSAGE      \
-    1$:               \
+    LABEL:            \
 __endasm;
+#define BGB_LOG(MESSAGE) BGB_LOG_(LOG_UNIQUE_LABEL, MESSAGE)
 
 // Break in the BGB debugger
 #define BGB_BREAK     \
